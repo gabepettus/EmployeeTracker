b53f3561e7095fb8a38485f686d19711a9509ca3
gdpettus <gabe.pettus@gmail.com>
2019-11-25 21:20:57 -0700

    added main menu struture and main deptmenat menu struction with function calls

--- index.js
+++ index.js
@@ -1,66 +1,50 @@
 const inquirer = require("inquirer");
 
-const questions = require("./lib/questions")
+const questMenu = require("./lib/menu");
+const questDept= require("./lib/department");
+const questRole = require("./lib/roles");
+const questEmpl= require("./lib/employee");
 
-const test = false;
-
-// use inquire to ask general employee questions
-const askOptions = () => {
-  if (test) console.log("entered: askOptions", questions.options);
-  return inquirer.prompt(questions.options);
-};
+const test = true;
 
 async function init() {
   if (test) console.log("started init:");
-  // array in which to store employees
-  // let employee;
-
   // will need to wrap this in a loop for n-employees
   try {
     let again = true;
 
+    console.log("herevvv",questMenu,"^^^here");
+
     while (again) {
       // build employee information (i would put this in the Employee object)
-      const ansOptions = await askOptions();
-      // maybe these should be in the employee class?
+      const ansOptions = await questMenu();
+      if (test) { console.log("answers:",ansOptions); }
 
-      // increment id for next employee
-
-      switch (ansGenEmpl.role) {
-        case 'Intern':
-          ansRoleEmpl = await askIntEmpl();
-          employee = new Intern (fullName,id,email,ansRoleEmpl.school);
-          intList.push(employee);
+      switch (ansOptions.mainMenu) {
+        case 'Manage departments':
+          console.log("case md");
+          await questDept();
+          console.log("here");
+          // employee = new Intern (fullName,id,email,ansRoleEmpl.school);
+          // intList.push(employee);
           break;
-        case 'Engineer':
-          ansRoleEmpl = await askEngEmpl();
-          employee = new Engineer (fullName,id,email,ansRoleEmpl.github);
-          engList.push(employee);
+        case 'Manage roles':
+          console.log("case mr");
+          ansRoles = await questRole();
+          break;
+        case 'Manage employees':
+          console.log("case me");
+          ansEmpl = await questEmpl();
+          break;
+        default:
+          again = false;
           break;
       }
-
-      console.log(`Added new employee ${employee.getName()} to team!`);
-
-      // can i combine this into one statement
-      const askResult = await askAgain();
-      again = askResult.again;
     }
-
-    // sort lists 
-    manList.sort(nameCompare);
-    engList.sort(nameCompare);
-    intList.sort(nameCompare);
-
-    const file = generateHTML(teamList);
-
-    // write to file
-
-    const filename = "./index.html";
-    write2File(filename,file);
-
   } catch (error) {
     console.log(`There was a problem ${error}`);
   }
 }
 
 init();
+
